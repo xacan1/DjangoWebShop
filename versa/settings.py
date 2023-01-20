39ec47ea1e36e3393e5cd58defd6fcd21b7ab5ab
Hasan <xacan-mail@mail.ru>
2023-01-20 17:53:11 +0500

    fix settings

--- versa/settings.py
+++ versa/settings.py
@@ -150,6 +150,24 @@ REST_FRAMEWORK = {
     ],
 }
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_FILE_PATH  =  BASE_DIR / 'sent_emails'
+EMAIL_HOST = config.EMAIL_SMTP
+EMAIL_HOST_USER = config.EMAIL_USER
+EMAIL_HOST_PASSWORD = config.EMAIL_PASSWORD
+EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+
+ADMINS = [
+    ('Admin', 'xacan-mail@mail.ru'),
+]
+
+MANAGERS = ADMINS
+
 # LOGGING = {
 #     'version': 1,
 #     'disable_existing_loggers': False,
